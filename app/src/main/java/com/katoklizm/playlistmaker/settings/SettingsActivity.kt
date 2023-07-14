d31fdba72266b6804844bbfb8235deb23ce88449
Demonided <Demonided@mail.ru>
2023-07-14 14:32:32 +0300

    update project

--- app/src/main/java/com/katoklizm/playlistmaker/settings/SettingsActivity.kt
+++ app/src/main/java/com/katoklizm/playlistmaker/settings/SettingsActivity.kt
@@ -3,7 +3,6 @@ package com.katoklizm.playlistmaker.settings
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.katoklizm.playlistmaker.MainActivity
 import com.katoklizm.playlistmaker.R
 
 class SettingsActivity: AppCompatActivity() {
