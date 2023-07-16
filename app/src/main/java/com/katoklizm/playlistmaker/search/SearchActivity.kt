766b23cd6bd186a48af508ca4522240cc0d6cd6b
Katoklizm <DemoniDeD@mail.ru>
2023-07-16 23:11:45 +0300

    update

--- app/src/main/java/com/katoklizm/playlistmaker/search/SearchActivity.kt
+++ app/src/main/java/com/katoklizm/playlistmaker/search/SearchActivity.kt
@@ -1,24 +1,27 @@
 package com.katoklizm.playlistmaker.search
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.katoklizm.playlistmaker.R
+import kotlin.random.Random
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val message = "Привет, Android-разработка это круто"
-        val shareIntent = Intent(Intent.ACTION_SENDTO)
-        shareIntent.data = Uri.parse("mailto:")
-        shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("yourEmail@ya.ru"))
-        shareIntent.putExtra(Intent.EXTRA_TEXT, message)
-        startActivity(shareIntent)
+        val liner = findViewById<LinearLayout>(R.id.btn_hs)
+
+        liner.setOnClickListener {
+            val color = Color.argb(255, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+            liner.display
+        }
     }
 }
 
