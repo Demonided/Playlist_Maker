4bb242f634a195ccf6bf43e1f690e6b886e9a0e0
Demonided <Demonided@mail.ru>
2023-07-17 12:08:33 +0300

    sda

--- app/src/main/java/com/katoklizm/playlistmaker/search/SearchActivity.kt
+++ app/src/main/java/com/katoklizm/playlistmaker/search/SearchActivity.kt
@@ -1,21 +1,51 @@
 package com.katoklizm.playlistmaker.search
 
-import android.content.Intent
-import android.graphics.Color
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import com.katoklizm.playlistmaker.R
-import kotlin.random.Random
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
     }
 }
+
+//        val clearInputField = findViewById<ImageView>(R.id.clear_text_search)
+//        val editTextSearch = findViewById<EditText>(R.id.edit_text_search)
+//
+//
+//        clearInputField.setOnClickListener {
+//            editTextSearch.setText("")
+//        }
+
+//        val simpleTextWatcher = object : TextWatcher {
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                // empty
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                clearInputField.visibility = clearButtonVisibility(s)
+//            }
+//
+//            override fun afterTextChanged(s: Editable?) {
+//                // empty
+//            }
+//        }
+
+//    }
+
+//    private fun clearButtonVisibility(s: CharSequence?): Int {
+//        return if (s.isNullOrEmpty()) {
+//            View.GONE
+//        } else {
+//            View.VISIBLE
+//        }
+//    }
+//}
 
